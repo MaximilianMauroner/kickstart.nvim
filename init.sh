348f7f3169a37feff2156d535dcd46d4cd2f2d31
Maximilian Mauroner <github@relay.mauroner.eu>
2024-10-08 16:37:40 +0200

    setup updated

--- init.sh
+++ init.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y software-properties-common
+# Function to install Ansible
+install_ansible() {
+    sudo apt update
+    sudo apt install -y software-properties-common
+    
+    # Add Ansible PPA and install Ansible
+    sudo add-apt-repository --yes --update ppa:ansible/ansible
+    sudo apt install -y ansible
+}
 
-# Add Ansible PPA and install Ansible
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
-
-ansible-playbook setup.yml --ask-become-pass
+# Check for arguments
+if [ "$#" -gt 0 ]; then
+    echo "Running only the Ansible playbook..."
+    ansible-playbook setup.yml --ask-become-pass
+else
+    echo "Installing Ansible..."
+    install_ansible
+    echo "Running the Ansible playbook..."
+    ansible-playbook setup.yml --ask-become-pass
+fi
